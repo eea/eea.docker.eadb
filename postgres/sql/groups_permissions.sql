a4a46a3c8daa8391a12ad37dfbdd2cd44354db10
michimau <mauro.michielon@eea.europa.eu>
2016-08-15 14:01:31 +0200

    Update groups_permissions.sql

--- postgres/sql/groups_permissions.sql
+++ postgres/sql/groups_permissions.sql
@@ -1,6 +1,8 @@
+GRANT CONNECT ON DATABASE ea TO gr_ea_editors_ldap;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO gr_ea_editors_ldap;
 GRANT SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO gr_ea_editors_ldap;
 
+GRANT CONNECT ON DATABASE ea TO gr_ea_viewers_ldap;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO gr_ea_viewers_ldap;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO gr_ea_viewers_ldap;
 
